9c222d6dbe68f94f0b4544aa1a8dbf4379e0e06e
sontus-tripura <sontustrp.com@gmail.com>
2020-07-23 12:46:00 +0600

    onclick event tested

--- src/components/Functional.js
+++ src/components/Functional.js
@@ -2,9 +2,14 @@ import React from "react";
 
 
 function myfun(props) {
+    function doThis() {
+       alert("button working..")
+    }
+
 return(
 
     <div>
+    <button onClick={ doThis }>Click </button>
     <h2> I am { props.name } and age: { props.age } </h2>
 
     </div>
--- src/components/Index.js
+++ src/components/Index.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 
 class Index extends Component {
+
+    doThis(){
+        alert("this button cliked from class button")
+    }
     render() {
         return (
            <div>
 
-
+           <button onClick={ this.doThis }> button from class </button>
             <h2> Hell I am { this.props.name } and { this.props.age} years old</h2>
 
 
