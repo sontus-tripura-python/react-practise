cbfd89dff4888fe469c8d08c029075a6c0997b7d
sontus-tripura <sontustrp.com@gmail.com>
2020-07-23 12:16:00 +0600

    class component added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Index from "./components/Index";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Welcome to my to my website
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with sontus tripura
-        </a>
-      </header>
+
+
+        <Index></Index>
+
+
+
+
+
+
+
+
+
     </div>
   );
 }
---
+++ src/components/Index.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class Index extends Component {
+    render() {
+        return (
+           <div>
+            <h1> heding one </h1>
+
+            <h2> Sontus tripura </h2>
+
+       <p> I am from fuctional </p>
+
+            </div>
+
+    )
+        ;
+    }
+}
+
+export default Index;
